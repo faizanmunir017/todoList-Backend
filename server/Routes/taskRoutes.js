a375241102e94de20dc055917960552498768871
Faizan Munir <faizanmunir017@gmail.com>
2024-11-07 19:13:11 +0500

    Controller for toggle created

--- server/Routes/taskRoutes.js
+++ server/Routes/taskRoutes.js
@@ -7,5 +7,6 @@ router.post("/tasks", taskController.addTask);
 router.get("/tasks", taskController.getTasks);
 
 router.delete("/tasks/:id", taskController.deleteTask);
+router.patch("/tasks/:id/toggle", taskController.toggleTask);
 
 module.exports = router;
--- server/controllers/taskControllers.js
+++ server/controllers/taskControllers.js
@@ -32,3 +32,18 @@ exports.deleteTask = async (req, res) => {
     res.status(500).json({ message: "Failed to delete task", error });
   }
 };
+
+exports.toggleTask = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const task = await User.findById(id);
+    if (!task) {
+      return res.status(404).json({ message: "Task not found" });
+    }
+    task.completed = !task.completed;
+    await task.save();
+    res.status(200).json(task);
+  } catch (error) {
+    res.status(500).json({ message: "Failed to toggle task", error });
+  }
+};
