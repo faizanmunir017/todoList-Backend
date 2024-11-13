0b169caf61c609374ba4db2f98fe88c6b794b8bb
Faizan Munir <faizanmunir017@gmail.com>
2024-11-13 19:26:09 +0500

    Code refactored

--- server/controllers/taskControllers.js
+++ server/controllers/taskControllers.js
@@ -17,12 +17,10 @@ exports.addTask = async (req, res) => {
 
     const token =
       req.headers.authorization && req.headers.authorization.split(" ")[1];
-    // console.log("token add tasks:", token);
     const { id: userId } = getUserFromToken(token);
 
     const newTask = new Task({ name, completed, userId });
     if (!userId) {
-      console.log("user Id error");
       return res.status(400).json({ message: "User ID is missing or invalid" });
     }
     await newTask.save();
@@ -36,19 +34,16 @@ exports.addTask = async (req, res) => {
 exports.getTasks = async (req, res) => {
   try {
     if (!req.headers.authorization) {
-      console.log("problem in header");
       return res.status(401).json({ message: "Authorization header missing" });
     }
     const token =
       req.headers.authorization && req.headers.authorization.split(" ")[1];
-    // console.log("token get tasks:", token);
+
     const { id: userId } = getUserFromToken(token);
-    // console.log(token);
     const tasks = await Task.find({ userId });
     res.status(200).json(tasks);
   } catch (error) {
-    console.log(error);
-    res.status(500).json({ message: "Failed to retrieve tasks", error });
+    return res.status(500).json({ message: "Failed to retrieve tasks", error });
   }
 };
 
@@ -90,7 +85,6 @@ exports.editTask = async (req, res) => {
     res.status(200).json(task);
 
     if (!task) {
-      console.log("here");
       return res.status(404).json({ message: "Task not found" });
     }
   } catch (error) {
--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -11,15 +11,13 @@ exports.register = async (req, res) => {
     const oldUser = await users.findOne({ email });
 
     if (oldUser) {
-      console.log("user already exists");
       return res.status(400).send({ error: "User already exists" });
     }
     const newUser = new users({ email, password: encryptedPassword });
     await newUser.save();
     res.status(201).json(newUser);
   } catch (error) {
-    console.error("Error adding task:", error);
-    res.status(500).json({ message: "Failed to register user " });
+    return res.status(500).json({ message: "Failed to register user " });
   }
 };
 
@@ -29,13 +27,11 @@ exports.login = async (req, res) => {
     const user = await users.findOne({ email });
 
     if (!user) {
-      console.log("user with this email does not exist");
       return res.status(400).send({ error: "User does not exist" });
     }
 
     if (await bycrypt.compare(password, user.password)) {
       const token = jwt.sign({ id: user._id }, JWT_SECRET, { expiresIn: "1h" });
-      // console.log("Token in :", token);
 
       if (res.status(201)) {
         return res.json({
@@ -56,7 +52,6 @@ exports.login = async (req, res) => {
     }
     res.status(400).send({ error: "invalid password entered" });
   } catch (error) {
-    console.log(error);
-    res.status(500).json({ message: "Failed to login User" });
+    return res.status(500).json({ message: "Failed to login User" });
   }
 };
